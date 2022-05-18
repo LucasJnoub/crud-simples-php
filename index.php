73e0c9975ac7480991cd6a328021ffbf05718b51
LucasJnoub <66445617+LucasJnoub@users.noreply.github.com>
2022-05-18 20:48:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!--forms para atualizar -->
+<form action="index.php" method="post">    
+    Digite o id e o novo nome da pessoa que deseja atualizar: <br>
+    ID: <input type="text" name="idAtualizar"><br>
+    Novo nome: <input type="text" name="novo_nome"> <br>
+    Novo email: <input type="text" name="novo_email"> <br>
+    <input type="submit" value="atualizar">
+</form>
+
+<hr>
+<!--forms para deletar -->
+<form action="index.php" method="post">
+    Digite o id q deseja deletar:
+    <input type="text" name="id"><br>
+    <input type="submit" value="deletar">
+</form>
+
 <?php
 
  $db_user = 'root';
@@ -9,30 +26,33 @@
  } catch (Exception $error) {
      echo "Ocorreu o seguinte erro:".$error->getMessage();
  }
-
  //Inserindo dados na tabela clientes.
-
+ // O usuário é inserido automaticamente.
  $sql = 'INSERT INTO clientes (nome,email,cidade,estado) VALUES(:nome,:email,:cidade,:estado)';
  $result = $connection->prepare($sql);
  $result->bindValue(':nome','Lucas Oliveira');
  $result->bindValue(':email','lucas.jnoub@gmail.com');
  $result->bindValue(':cidade','Rio de Janeiro');
  $result->bindValue(':estado','RJ');
-
  $result->execute();
+ 
 
 
  //atualizando dados na tabela clientes.
-
-$sql = 'UPDATE clientes SET nome="Lucas Silva" WHERE id =:id';
+$novo_nome = $_POST["novo_nome"];
+$idAtualizado = $_POST["idAtualizar"];
+$novo_email = $_POST["novo_email"];
+$sql = "UPDATE clientes SET nome='$novo_nome', email='$novo_email' WHERE id =:id";
 $result = $connection->prepare($sql);
-$result->bindValue(':id',2);
+$result->bindValue(':id',$idAtualizado);
 $result->execute();
 
+
 // deletando dados na tabela clientes.
-$sql = 'DELETE FROM clientes WHERE nome =:nome';
+$sql = 'DELETE FROM clientes WHERE id =:id';
 $result = $connection->prepare($sql);
-$result->bindValue(':nome','Lucas Silva');
+$id = $_POST['id'];
+$result->bindValue(':id',$id);
 $result->execute();
 
 // Consultando dados da tabela cientes.
